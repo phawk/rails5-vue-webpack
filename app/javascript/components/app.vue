f6ebb12dd907323183319be104316d5d59d7f4e7
Pete Hawkins <pete@phawk.co.uk>
2017-02-05 16:21:40 +0000

    Fetch contacts

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -6,12 +6,22 @@
 
 <script>
 const Vue = require("vue");
+const contacts = require("helpers/resources").contacts;
 
 module.exports = Vue.extend({
   data() {
     return {
+      contacts: [],
       hello: "world",
     };
+  },
+
+  created() {
+    contacts.get().then((resp) => {
+      this.contacts = resp.data;
+    }, (resp) => {
+      console.error("Error fetching contacts");
+    });
   },
 
   methods: {
